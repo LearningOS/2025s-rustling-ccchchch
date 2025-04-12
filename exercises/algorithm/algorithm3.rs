4d869adab8a2ee5c72cde3b3526e0e2b1e8b5250
ch-self-666 <chenhao20220623@163.com>
2025-04-12 12:03:23 +0800

    finish

--- exercises/algorithm/algorithm3.rs
+++ exercises/algorithm/algorithm3.rs
@@ -1,17 +1,32 @@
-/*
-	sort
-	This problem requires you to implement a sorting algorithm
-	you can use bubble sorting, insertion sorting, heap sorting, etc.
-*/
-// I AM NOT DONE
-
 fn sort<T: PartialOrd>(array: &mut [T]) {
     let len = array.len();
     for i in 0..len {
+        let mut swapped = false;
         for j in 0..len - i - 1 {
             if array[j] > array[j + 1] {
                 array.swap(j, j + 1);
+                swapped = true;
             }
         }
+        // 如果这一轮没有发生交换，说明数组已经有序
+        if !swapped {
+            break;
+        }
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_sort() {
+        let mut numbers = vec![4, 2, 7, 1, 3];
+        sort(&mut numbers);
+        assert_eq!(numbers, vec![1, 2, 3, 4, 7]);
+
+        let mut strings = vec!["banana", "apple", "cherry", "date"];
+        sort(&mut strings);
+        assert_eq!(strings, vec!["apple", "banana", "cherry", "date"]);
     }
 }
